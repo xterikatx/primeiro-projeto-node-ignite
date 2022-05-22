90b95273ceef74c11c17c01fe9be4bdc85f5a371
erika lopes <caluerika@gmail.com>
2022-05-22 16:05:14 -0300

    adicionado deposito

--- src/index.js
+++ src/index.js
@@ -48,4 +48,21 @@ app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
   return res.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const { customer } = req;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
+});
+
 app.listen(3333);
