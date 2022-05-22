5bc2dff929f7ad7423a096487d38000738e9d896
erika lopes <caluerika@gmail.com>
2022-05-22 16:30:44 -0300

    list por date

--- src/index.js
+++ src/index.js
@@ -96,4 +96,19 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
   return res.status(201).send();
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return res.json(statement);
+});
+
 app.listen(3333);
