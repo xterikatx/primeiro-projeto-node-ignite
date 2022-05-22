7b8606749ede378c45aab616f600affcf938e590
erika lopes <caluerika@gmail.com>
2022-05-22 15:42:19 -0300

    adicionando usuarios!

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.listen(3333);
